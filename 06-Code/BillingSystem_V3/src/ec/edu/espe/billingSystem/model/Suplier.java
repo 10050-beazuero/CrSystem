ac03d6f854e531892e2b8a9c5279d37d1b7bdbdd
Erika Carolina Alvarado Morales <75351106+Carolina-Alvarado@users.noreply.github.com>
2021-01-19 19:10:54 -0500

    Update Suplier.java

--- 06-Code/BillingSystem_V3/src/ec/edu/espe/billingSystem/model/Suplier.java
+++ 06-Code/BillingSystem_V3/src/ec/edu/espe/billingSystem/model/Suplier.java
@@ -16,7 +16,6 @@ public class Suplier {
     }
 
     public Suplier() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
         
     public void suplierConsult(){}
